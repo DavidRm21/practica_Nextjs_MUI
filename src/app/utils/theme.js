040581fe1d2499a366c8e11b662f521830e53326
Cristian Romero <cdavidromero214@gmail.com>
2024-04-03 20:52:08 -0500

    add theme

---
+++ src/app/utils/theme.js
@@ -0,0 +1,46 @@
+'use client';
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+
+    palette:{
+        primary: {
+            main: "#1D5E69",
+        },
+        secondary: {
+            main: "#FA3419",
+        },
+        grey_text: {
+            main: "#778089",
+        },
+        grey: {
+            main: "#EAEAEA",
+        },
+    },
+
+    typography: {
+        h1: {
+            fontSize: "20px",
+            color: "black"
+        },
+        h2: {
+            fontSize: "18px",
+            color: "black"
+        },
+        h3: {
+            fontSize: "16px",
+            color: "black"
+        },
+        h4: {
+            fontSize: "14px",
+            color: "black"
+        },
+        h5: {
+            fontSize: "12px",
+            color: "black"
+        },
+    },
+
+})
+
+export default theme;
